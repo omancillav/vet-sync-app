dded1ef0a5d729b114f222257b3efb4e0bf2da0a
omancillav <edgarmncllav04@gmail.com>
2025-06-16 21:59:04 -0600

    improve theme handle

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from '@/components/theme-provider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <App />
     </ThemeProvider>
   </StrictMode>
