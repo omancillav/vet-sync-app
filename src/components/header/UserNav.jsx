eb5b5f577596093cb708800b731b907a9660ff11
omancillav <edgarmncllav04@gmail.com>
2025-06-18 02:17:40 -0600

    add user lastname to dropdown menu

--- src/components/header/UserNav.jsx
+++ src/components/header/UserNav.jsx
@@ -30,7 +30,7 @@ export function UserNav() {
         <DropdownMenuContent className="w-56" align="end" forceMount>
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">{user?.nombre}</p>
+              <p className="text-sm font-medium leading-none">{user?.nombre + ' ' + user?.apellido}</p>
               <p className="text-xs leading-none text-muted-foreground">{user?.email}</p>
             </div>
           </DropdownMenuLabel>
