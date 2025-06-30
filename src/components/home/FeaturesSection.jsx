40debc6c66785fbbeeb88b301f1d469c8b6dabdb
omancillav <edgarmncllav04@gmail.com>
2025-06-29 23:40:56 -0600

    home lazy loading

--- src/components/home/FeaturesSection.jsx
+++ src/components/home/FeaturesSection.jsx
@@ -75,7 +75,7 @@ export function FeaturesSection() {
           </p>
         </div>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 md:gap-8 gap-4">
           {features.map((feature, index) => (
             <Card key={index}>
               <CardHeader>
