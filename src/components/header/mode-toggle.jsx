86a9749ccf33f3e821aa2e38059bd9425320124b
omancillav <edgarmncllav04@gmail.com>
2025-07-26 01:29:15 -0600

    fix: update button icon sizes in Header and ModeToggle components

--- src/components/header/mode-toggle.jsx
+++ src/components/header/mode-toggle.jsx
@@ -14,9 +14,9 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className='hover:cursor-pointer dark:hover:text-white'>
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="ghost" size="icon" className="hover:cursor-pointer dark:hover:text-white">
+          <Sun className="!h-5 !w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute !h-6 !w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
