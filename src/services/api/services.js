5f450586fd12321a7ab9d1c33cbce8224aa8868c
omancillav <edgarmncllav04@gmail.com>
2025-07-28 02:00:25 -0600

    fix: update ServicesSkeleton to hide certain elements on smaller screens and adjust TabsList styling for dark mode

--- src/services/api/services.js
+++ src/services/api/services.js
@@ -1,6 +1,8 @@
 import api from './client'
 
 export const getServices = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 5000))
+
   try {
     const response = await api.get('/services/active/true')
     return response.data
