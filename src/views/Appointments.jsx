1adec600f72d005a6f9fdd63ac67b20639c11920
omancillav <edgarmncllav04@gmail.com>
2025-07-30 23:26:03 -0600

    feat: add toast notification for 'Agendar Cita' button in ServicesCard and Appointments components

--- src/views/Appointments.jsx
+++ src/views/Appointments.jsx
@@ -56,7 +56,10 @@ export function Appointments() {
           </section>
           <section className="w-full md:w-1/2 md:flex md:justify-end">
             {isAuthenticated && (
-              <Button onClick={() => toast.warning('Funcionalidad de agendar cita en desarrollo')} className="w-full md:w-auto">
+              <Button
+                onClick={() => toast.warning('Funcionalidad de agendar cita en desarrollo')}
+                className="w-full md:w-auto"
+              >
                 Agendar Cita
                 <CalendarPlus className="h-4 w-4 ml-2" />
               </Button>
