93532536c873fa6cec147bb4453eb32f05b0177c
omancillav <edgarmncllav04@gmail.com>
2025-08-11 11:05:42 -0600

    refactor: add DataTable component and integrate appointment data display

---
+++ src/components/appointments/table/columns.jsx
@@ -0,0 +1,27 @@
+export const columns = [
+  {
+    accessorKey: 'fecha',
+    header: 'Fecha'
+  },
+  {
+    accessorKey: 'hora_inicio',
+    header: 'Hora Inicio'
+  },
+  {
+    accessorKey: 'nombre_mascota',
+    header: 'Mascota'
+  },
+
+  {
+    accessorKey: 'nombre_profesional',
+    header: 'Profesional'
+  },
+  {
+    accessorKey: 'nombre_servicio',
+    header: 'Servicio'
+  },
+  {
+    accessorKey: 'status',
+    header: 'Estado'
+  }
+]
--- src/views/Appointments.jsx
+++ src/views/Appointments.jsx
@@ -1,30 +1,30 @@
-import { useEffect } from "react";
-import { useAuth } from "@/hooks/useAuth";
-import { AuthPrompt } from "@/components/AuthPrompt";
-import { NoAppointments } from "@/components/appointments/NoAppointments";
-import { useAppointments } from "@/hooks/useAppointments";
-import { LoadingSpinner } from "@/components/loaders/LoadingSpinner.jsx";
-import { ErrorCard } from "@/components/ErrorCard";
-import { Button } from "@/components/ui/button";
-import { CalendarPlus } from "lucide-react";
-import { toast } from "sonner";
-import { Separator } from "@/components/ui/separator";
-import { AppointmentCard } from "../components/appointments/AppointmentCard";
-import { sortAppointmentsByDate, groupAppointmentsByStatus } from "@/lib/utils.js";
+import { useEffect } from 'react'
+import { useAuth } from '@/hooks/useAuth'
+import { AuthPrompt } from '@/components/AuthPrompt'
+import { NoAppointments } from '@/components/appointments/NoAppointments'
+import { useAppointments } from '@/hooks/useAppointments'
+import { LoadingSpinner } from '@/components/loaders/LoadingSpinner.jsx'
+import { ErrorCard } from '@/components/ErrorCard'
+import { Button } from '@/components/ui/button'
+import { CalendarPlus } from 'lucide-react'
+import { toast } from 'sonner'
+import { columns } from '@/components/appointments/table/columns'
+import { DataTable } from '@/components/appointments/table/data-table'
+import { sortAppointmentsByDate } from '@/lib/utils.js'
 
 export function Appointments() {
-  const { appointments, noAppointments, loading, error, initializeAppointments } = useAppointments();
-  const { isAuthenticated } = useAuth();
+  const { appointments, noAppointments, loading, error, initializeAppointments } = useAppointments()
+  const { isAuthenticated } = useAuth()
 
   useEffect(() => {
     if (isAuthenticated && !loading) {
-      initializeAppointments();
+      initializeAppointments()
     }
-  }, [isAuthenticated, loading, initializeAppointments]);
+  }, [isAuthenticated, loading, initializeAppointments])
 
   const renderContent = () => {
     if (!isAuthenticated) {
-      return <AuthPrompt icon="📅" message="Debes iniciar sesión para agendar y ver tus citas" />;
+      return <AuthPrompt icon="📅" message="Debes iniciar sesión para agendar y ver tus citas" />
     }
 
     if (loading) {
@@ -32,46 +32,25 @@ export function Appointments() {
         <div className="flex justify-center items-center h-64">
           <LoadingSpinner />
         </div>
-      );
+      )
     }
 
     if (error) {
-      return <ErrorCard message="No se pudieron cargar tus citas. Inténtalo de nuevo." />;
+      return <ErrorCard message="No se pudieron cargar tus citas. Inténtalo de nuevo." />
     }
 
-    if (noAppointments) return <NoAppointments />;
+    if (noAppointments) return <NoAppointments />
 
-    const sortedAppointments = sortAppointmentsByDate(appointments);
-    const { pending, history } = groupAppointmentsByStatus(sortedAppointments);
-    const hasPending = pending.length > 0;
-    const hasHistory = history.length > 0;
+    const sortedAppointments = sortAppointmentsByDate(appointments)
 
     return (
       <>
-        {hasPending && (
-          <section>
-            <h2 className="text-2xl font-bold mb-4">Citas Pendientes</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
-              {pending.map((appointment) => (
-                <AppointmentCard key={appointment.id} appointment={appointment} />
-              ))}
-            </div>
-          </section>
-        )}
-        {hasPending && hasHistory && <Separator className="my-4 md:my-8" />}
-        {hasHistory && (
-          <section>
-            <h2 className="text-2xl font-bold mb-4">Historial de Citas</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
-              {history.map((appointment) => (
-                <AppointmentCard key={appointment.id} appointment={appointment} isPast={true} />
-              ))}
-            </div>
-          </section>
-        )}
+        <div className="container mx-auto py-10">
+          <DataTable columns={columns} data={sortedAppointments} />
+        </div>
       </>
-    );
-  };
+    )
+  }
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="max-w-6xl mx-auto">
@@ -83,7 +62,7 @@ export function Appointments() {
           <section className="w-full md:w-1/2 md:flex md:justify-end">
             {isAuthenticated && (
               <Button
-                onClick={() => toast.warning("Funcionalidad de agendar cita en desarrollo")}
+                onClick={() => toast.warning('Funcionalidad de agendar cita en desarrollo')}
                 className="w-full md:w-auto"
               >
                 Agendar Cita
@@ -95,5 +74,5 @@ export function Appointments() {
         {renderContent()}
       </div>
     </div>
-  );
+  )
 }
